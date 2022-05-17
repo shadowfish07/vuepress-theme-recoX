f2affffdaf699eaea5f8733c267691026743d440
shadowfish07 <38303418+shadowfish07@users.noreply.github.com>
2022-05-18 01:26:29 +0800

    添加访问记录

--- packages/vuepress-theme-recox/enhanceApp.js
+++ packages/vuepress-theme-recox/enhanceApp.js
@@ -1,23 +1,19 @@
 /* eslint-disable no-proto */
-import postMixin from '@theme/mixins/posts'
-import localMixin from '@theme/mixins/locales'
-import { interceptRouterError, fixRouterError404 } from '@theme/helpers/other'
-import { install } from 'vue-demi'
+import postMixin from "@theme/mixins/posts";
+import localMixin from "@theme/mixins/locales";
+import {
+  interceptRouterError,
+  fixRouterError404,
+  visitRecord,
+} from "@theme/helpers/other";
+import { install } from "vue-demi";
 
-export default ({
-  Vue,
-  siteData,
-  isServer,
-  router
-}) => {
-  install(Vue)
-  Vue.mixin(postMixin)
-  Vue.mixin(localMixin)
-  // if (!isServer) {
-  //   addScriptToHead('//kit.fontawesome.com/51b01de608.js')
-  //   registerCodeThemeCss(siteData.themeConfig.codeTheme)
-  // }
+export default ({ Vue, siteData, isServer, router }) => {
+  install(Vue);
+  Vue.mixin(postMixin);
+  Vue.mixin(localMixin);
 
-  interceptRouterError(router)
-  fixRouterError404(router)
-}
+  interceptRouterError(router);
+  fixRouterError404(router);
+  visitRecord(siteData.themeConfig.vuePressAdminHost, router);
+};
--- packages/vuepress-theme-recox/helpers/other.js
+++ packages/vuepress-theme-recox/helpers/other.js
@@ -1,56 +1,63 @@
 /* eslint-disable no-proto */
-import { addLinkToHead } from './utils'
-export function getOneColor () {
+import { addLinkToHead } from "./utils";
+import axios from "axios";
+export function getOneColor() {
   const tagColorArr = [
-    '#e15b64',
-    '#f47e60',
-    '#f8b26a',
-    '#abbd81',
-    '#849b87',
-    '#e15b64',
-    '#f47e60',
-    '#f8b26a',
-    '#f26d6d',
-    '#67cc86',
-    '#fb9b5f',
-    '#3498db'
-  ]
-  const index = Math.floor(Math.random() * tagColorArr.length)
-  return tagColorArr[index]
+    "#e15b64",
+    "#f47e60",
+    "#f8b26a",
+    "#abbd81",
+    "#849b87",
+    "#e15b64",
+    "#f47e60",
+    "#f8b26a",
+    "#f26d6d",
+    "#67cc86",
+    "#fb9b5f",
+    "#3498db",
+  ];
+  const index = Math.floor(Math.random() * tagColorArr.length);
+  return tagColorArr[index];
 }
 
-export function registerCodeThemeCss (theme = 'tomorrow') {
-  const themeArr = ['tomorrow', 'funky', 'okaidia', 'solarizedlight', 'default']
+export function registerCodeThemeCss(theme = "tomorrow") {
+  const themeArr = [
+    "tomorrow",
+    "funky",
+    "okaidia",
+    "solarizedlight",
+    "default",
+  ];
   const href = `//prismjs.com/themes/prism${
-    themeArr.indexOf(theme) > -1 ? `-${theme}` : ''
-  }.css`
+    themeArr.indexOf(theme) > -1 ? `-${theme}` : ""
+  }.css`;
 
-  addLinkToHead(href)
+  addLinkToHead(href);
 }
 
-export function interceptRouterError (router) {
+export function interceptRouterError(router) {
   // 获取原型对象上的 push 函数
-  const originalPush = router.__proto__.push
+  const originalPush = router.__proto__.push;
   // 修改原型对象中的p ush 方法
-  router.__proto__.push = function push (location) {
-    return originalPush.call(this, location).catch((err) => err)
-  }
+  router.__proto__.push = function push(location) {
+    return originalPush.call(this, location).catch((err) => err);
+  };
 }
 
-export function fixRouterError404 (router) {
-  const routes = router.getRoutes()
+export function fixRouterError404(router) {
+  const routes = router.getRoutes();
   router.beforeEach((to, from, next) => {
     // 解决decode后反复重定向的问题
-    const route = routes.find((v) => v.regex.test(to.path))
+    const route = routes.find((v) => v.regex.test(to.path));
 
     // .html有重定向路由，无需特殊处理
     if (/\.html$/.test(to.path)) {
-      return next()
+      return next();
     }
 
     // 不特殊处理 非通配符的 没有重定向的路由
-    if (route && route.path !== '*' && !route.redirect) {
-      return next()
+    if (route && route.path !== "*" && !route.redirect) {
+      return next();
     }
 
     // 解决中文标签/分类路由无法被正确加载的问题
@@ -58,11 +65,30 @@ export function fixRouterError404 (router) {
       return next(
         Object.assign({}, to, {
           path: decodeURIComponent(to.path),
-          fullPath: decodeURIComponent(to.fullPath)
+          fullPath: decodeURIComponent(to.fullPath),
         })
-      )
+      );
     }
 
-    next()
-  })
+    next();
+  });
+}
+
+export function visitRecord(VPAHost, router) {
+  if (!VPAHost) return;
+  router.afterEach((to, from) => {
+    console.log(to);
+
+    axios.post(
+      VPAHost + "/api/statistics",
+      {
+        path: to.fullPath,
+        hostname: window.location.hostname,
+        ip: "0.0.0.0",
+      },
+      {
+        withCredentials: true,
+      }
+    );
+  });
 }
